4f6e1026d973be78921d304249b6225feb6e6a9e
DownTownScarf7 <vbence741@gmail.com>
2018-08-12 17:36:10 +0200

    feat(master): Added basic server

---
+++ src/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use('/public', express.static('public'));
+app.use(__dirname, express.static(__dirname));
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
