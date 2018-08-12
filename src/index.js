4acb24d2861c890a719cc8750db4038b79650662
DownTownScarf7 <vbence741@gmail.com>
2018-08-12 19:47:29 +0200

    feat(master): Added optionsbar handler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ const select = target => document.querySelector(target);
 const selectAll = target => document.querySelectorAll(target);
 const create = target => document.createElement(target);
 
+class HandlerMain {
+  constructor() {
+    this.buttons = {
+      login: null,
+      register: null
+    }
+  }
+
+  assignButtons() {
+    this.buttons.login = select('#btn-login');
+    this.buttons.register = select('#btn-register');
+  }
+
+  hideButton(button) {
+    this.buttons[button].style.display = 'none';
+  }
+
+  showButton(button) {
+    this.buttons[button].style.display = 'block';
+  }
+}
+
 class HandlerAdmin {
   constructor() {
     this.btn = null;
@@ -24,5 +46,7 @@ class HandlerAdmin {
 
 window.onload = () => {
   const handlerAdmin = new HandlerAdmin();
+  const handlerMain = new HandlerMain();
   handlerAdmin.placeElements('#options-links');
+  handlerMain.assignButtons();
 }
