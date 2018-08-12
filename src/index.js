948eeaa85e039b6ba201f8e9bf0b7992b5780076
DownTownScarf7 <vbence741@gmail.com>
2018-08-12 18:58:59 +0200

    feat(master): Added DOM admin button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const select = target => document.querySelector(target);
+const selectAll = target => document.querySelectorAll(target);
+const create = target => document.createElement(target);
+
+class HandlerAdmin {
+  constructor() {
+    this.btn = null;
+    this.btnDiv = null;
+    this.createButton();
+  }
+
+  createButton() {
+    this.btn = create('a');
+    this.btnDiv = create('div');
+    this.btnDiv.appendChild(this.btn);
+    this.btn.href = './admin';
+    this.btn.innerText = 'Admin options';
+  }
+
+  placeElements(target) {
+    select(target).appendChild(this.btnDiv);
+  }
+}
+
 window.onload = () => {
-  console.log('Testing SCRIPT');
+  const handlerAdmin = new HandlerAdmin();
+  handlerAdmin.placeElements('#options-links');
 }
