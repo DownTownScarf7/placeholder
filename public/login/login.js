0360023d0a39ca48bb5b207aaad38c15954ed9f7
DownTownScarf7 <vbence741@gmail.com>
2018-08-17 16:42:29 +0200

    feat(master): Copy-pasta login page

--- public/login/login.js
+++ public/login/login.js
@@ -1,1 +1,25 @@
-console.log('JS working');
+window.onload = () => {
+  const un = document.querySelector('#username');
+  const pw = document.querySelector('#password');
+  const show = document.querySelector('img');
+  
+  document.querySelector('form').addEventListener('click', event => {
+    switch (event.target) {
+      case document.querySelector('#submit'):
+        if (un.value && pw.value) {
+          event.preventDefault();
+          window.location.replace('http://localhost:8080');
+        }
+        break;
+      case show:
+        if (pw.type === 'password') {
+          pw.type = 'text';
+          show.src = 'https://cdn3.iconfinder.com/data/icons/show-and-hide-password/100/show_hide_password-08-512.png';
+        } else {
+          pw.type = 'password';
+          show.src = 'https://cdn3.iconfinder.com/data/icons/show-and-hide-password/100/show_hide_password-07-512.png';
+        }
+        break;
+    }
+  });
+}
