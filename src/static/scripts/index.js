d9d0f9c62bc4531cea80a1722aa19e8229ee7298
DownTownScarf7 <vbence741@gmail.com>
2018-09-25 20:16:41 +0200

    feat(dts-1): Major improvements on index page, working DOM actions through database

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -2,12 +2,38 @@ const server = 'http://localhost:8080';
 
 const select = target => document.querySelector(target);
 const create = tag => document.createElement(tag);
+const setText = (target, text) => select(target).innerText = text;
 
 window.onload = () => {
+  const displayCard = (card) => {
+    setText('#card-name', card.name);
+    setText('#card-desc', card.description);
+    select('#card-img').src = card.image;
+    select('#purchase').href = `${server}/purchase?card=${card.id}`;
+    select('#reload').href = `${server}/refill?card=${card.id}`;
+  }
+
   const assignCards = (cards) => {
     const selector = select('#card-selector');
+
+    displayCard(cards.find(card => card.avaible));
+    select('#card').removeChild(select('.loader-wrapper'));
+
     cards.forEach((card) => {
-      selector.appendChild(create('option')).innerText = card.name;
+      console.log('card: ', card);
+      if (card.avaible) {
+        const option = create('option');
+        option.innerText = card.name;
+        selector.appendChild(option);
+      }
+    });
+
+    selector.addEventListener('change', (evnt) => {
+      const selectedCard = cards.find(card => card.name === evnt.target.value);
+
+      if (selectedCard) {
+        displayCard(selectedCard);
+      }
     });
   };
 
